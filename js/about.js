4b52afb4c7bf57640f559f4bf96c67fee74c2c41
mahendra761 <mahendragawande90@gmail.com>
2023-04-29 11:56:24 +0530

    first commit

---
+++ js/about.js
@@ -0,0 +1,10 @@
+let counts=setInterval(updated);
+        let upto=0;
+        function updated(){
+            var count= document.getElementById("counter");
+            count.innerHTML=++upto;
+            if(upto===1000)
+            {
+                clearInterval(counts);
+            }
+        }
